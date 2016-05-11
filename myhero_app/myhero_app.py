1e584ac9165b200812ecee8593d8df083c249743
Hank Preston <hank.preston@gmail.com>
2016-05-11 18:35:32 -0400

    Moved vote submission logic to publish to an MQTT Topic.

--- myhero_app/myhero_app.py
+++ myhero_app/myhero_app.py
@@ -12,10 +12,18 @@ from flask import Flask, make_response, request, jsonify, Response
 import datetime
 import urllib
 import json
-import os, sys
+import os, sys, socket, dns.resolver
 import requests
+import paho.mqtt.publish as publish
 
 app = Flask(__name__)
+
+mqtt_host = ""
+mqtt_port = 0
+
+# Setup MQTT Topic Info
+lhost = socket.gethostname()
+
 
 # TODO - Decide if this will be maintaned going forward
 @app.route("/hero_list")
@@ -27,37 +35,34 @@ def hero_list():
     resp = make_response(jsonify(heros=hero_list))
     return resp
 
-# TODO - Remove GET version once Web Service is Updated
-@app.route("/vote/<hero>", methods=["GET", "POST"])
+@app.route("/vote/<hero>", methods=["POST"])
 def vote(hero):
-    if request.method == "GET":
-        u = urllib.urlopen(data_server + "/vote/" + hero)
-        page = u.read()
-        result = json.loads(page)['result']
-        if (result == "1"):
-            resp = make_response(jsonify(result="vote accepted"))
-        else:
-            resp = make_response(jsonify(result="vote rejected"))
-        return resp
     if request.method == "POST":
         # Verify that the request is propery authorized
         authz = valid_request_check(request)
         if not authz[0]:
             return authz[1]
-        u = data_server + "/vote/" + hero
-        data_requests_headers = {"key": data_key}
-        page = requests.post(u, headers = data_requests_headers)
-        result = page.json()["result"]
+
+        print request.headers
+
+        # Publish Message
+        publish_vote(hero)
+        result = "1"
+        # u = data_server + "/vote/" + hero
+        # data_requests_headers = {"key": data_key}
+        # page = requests.post(u, headers = data_requests_headers)
+        # result = page.json()["result"]
         print result
         if (result == "1"):
-            msg = {"result":"vote accepted"}
+            msg = {"result":"vote submitted"}
         else:
-            msg = {"result":"vote rejected"}
+            msg = {"result":"vote submission failed"}
         status = 200
         resp = Response(
             json.dumps(msg, sort_keys=True, indent=4, separators=(',', ': ')),
             content_type='application/json',
             status=status)
+
         return resp
 
 # TODO - Add Authentication
@@ -181,6 +186,41 @@ def valid_request_check(request):
         resp = Response(json.dumps(error), content_type='application/json', status=status)
         return (False, resp)
 
+def publish_vote(vote):
+    # Basic Publish to a MQTT Queue
+    print("Publishing vote.")
+    publish.single("MyHero-Votes/" + lhost, payload=vote, hostname=mqtt_host, port=mqtt_port, retain=True)
+    return ""
+
+# Get SRV Lookup Details for Queueing Server
+# The MQTT Server details are expected to be found by processing an SRV Lookup
+# The target will be consul for deploments utilizing Mantl.io
+# The underlying host running the service must have DNS servers configured to
+# Resolve the lookup
+def srv_lookup(name):
+    resolver = dns.resolver.Resolver()
+    results = []
+    try:
+        for rdata in resolver.query(name, 'SRV'):
+            results.append((str(rdata.target), rdata.port))
+        # print ("Resolved Service Location as {}".format(results))
+    except:
+        raise ValueError("Can't find SRV Record")
+    return results
+
+# Get IP for Host
+def ip_lookup(name):
+    resolver = dns.resolver.Resolver()
+    results = ""
+    try:
+        for rdata in resolver.query(name, 'A'):
+            results = str(rdata)
+        # print ("Resolved Service Location as {}".format(results))
+    except:
+        raise ValueError("Can't find A Record")
+    return results
+
+
 
 if __name__=='__main__':
     from argparse import ArgumentParser
@@ -193,6 +233,15 @@ if __name__=='__main__':
     )
     parser.add_argument(
         "-s", "--appsecret", help="App Server Key Expected in API Calls", required=False
+    )
+    parser.add_argument(
+        "-q", "--mqttserver", help="MQTT Server FQDN for SRV Lookup", required=False
+    )
+    parser.add_argument(
+        "-i", "--mqtthost", help="MQTT Server Host IP Address", required=False
+    )
+    parser.add_argument(
+        "-p", "--mqttport", help="MQTT Server Port", required=False
     )
 
     args = parser.parse_args()
@@ -233,5 +282,35 @@ if __name__=='__main__':
     print "App Server Key: " + app_key
     sys.stderr.write("App Server Key: " + app_key + "\n")
 
-    app.run(debug=True, host='0.0.0.0', port=int("5000"))
+    # To find the MQTT Server, two options are possible
+    # In order of priority
+    # 1.  Explicitly Set mqtthost and mqttport details from Arguments or Environment Variables
+    # 2.  Leveraging DNS to lookup an SRV Record to get HOST IP and PORT information
+    # Try #1 Option for Explicitly Set Options
+    mqtt_host = args.mqtthost
+    mqtt_port = args.mqttport
+    if (mqtt_host == None and mqtt_port == None):
+        mqtt_host = os.getenv("myhero_mqtt_host")
+        mqtt_port = os.getenv("myhero_mqtt_port")
+        if (mqtt_host == None and mqtt_port == None):
+            # Move onto #2 and Try DNS Lookup
+            mqtt_server = args.mqttserver
+            if (mqtt_server == None):
+                mqtt_server = os.getenv("myhero_mqtt_server")
+                if (mqtt_server == None):
+                    mqtt_server = raw_input("What is the MQTT Server FQDN for an SRV Lookup? ")
+            sys.stderr.write("MQTT Server: " + mqtt_server + "\n")
+            # Lookup and resolve the IP and Port for the MQTT Server
+            try:
+                records = srv_lookup(mqtt_server)
+                if len(records) != 1: raise Exception("More than 1 SRV Record Returned")
+                # To find the HOST IP address need to take the returned hostname from the
+                # SRV check and do an IP lookup on it
+                mqtt_host = str(ip_lookup(records[0][0]))
+                mqtt_port = records[0][1]
+            except ValueError:
+                raise ValueError("Message Queue Not Found")
+    sys.stderr.write("MQTT Host: %s \nMQTT Port: %s\n" % (mqtt_host, mqtt_port))
+
+    app.run(debug=True, host='0.0.0.0', port=int("5001"))
 
